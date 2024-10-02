9729695799a6151720b485e1bc72abf53bdda664
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2024-10-02 15:50:35 +0200

    tilføjet en ny film

--- src/main/java/com/example/kinoxp/KinoRepository.java
+++ src/main/java/com/example/kinoxp/KinoRepository.java
@@ -12,12 +12,19 @@ import java.util.List;
 
 public class KinoRepository {
     List<Movie> movies = List.of(new Movie(1L,
-            "Interstellar",
-            "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-            2015, "Christopher Nolan",
-            2.5,List.of("Matthew McConaughey"),
-            "https://static.posters.cz/image/750/plakater/interstellar-ice-walk-i23290.jpg",
-            List.of(Tags.Science_fiction, Tags.Drama, Tags.Alder15)));
+                    "Interstellar",
+                    "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                    2015, "Christopher Nolan",
+                    2.5, List.of("Matthew McConaughey"),
+                    "https://static.posters.cz/image/750/plakater/interstellar-ice-walk-i23290.jpg",
+                    List.of(Tags.Science_fiction, Tags.Drama, Tags.Alder15)),
+            new Movie(2L,
+                    "The Shawshank Redemption",
+                    "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                    1994, "Frank Darabont",
+                    2.2, List.of("Tim Robbins", "Morgan Freeman"),
+                    "https://image-url-for-shawshank-redemption.jpg",
+                    List.of(Tags.Drama, Tags.Alder15)));
 
 
     public List<Movie> getMovies() {
